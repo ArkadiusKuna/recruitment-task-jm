a6ccf34b842695cd7c7d30b4e808e7bd00625000
ArkadiusKuna <kuna.arek@gmail.com>
2022-12-13 22:02:20 +0100

    feat/react router setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,20 @@
+import { Routes, Route } from "react-router-dom";
+
+import HomePage from "./pages/HomePage/HomePage";
+import Continents from "./pages/Continents/Continents";
+import Countries from "./pages/Countries/Countries";
+import PageNotFound from "./pages/PageNotFound/PageNotFound";
+
 function App() {
   return (
-    <div>
-        Hello
-    </div>
+    <>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/continents" element={<Continents />} />
+        <Route path="/continent/:code" element={<Countries />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+    </> 
   );
 }
 
