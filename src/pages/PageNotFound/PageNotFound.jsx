d7d9bb51fed4a569b8cbeea5d3a307674ce3d228
ArkadiusKuna <kuna.arek@gmail.com>
2022-12-14 17:11:36 +0100

    feat/add  NotFoundPage styles

--- src/pages/PageNotFound/PageNotFound.jsx
+++ src/pages/PageNotFound/PageNotFound.jsx
@@ -1,6 +1,13 @@
+import Main from "layout/Main/Main";
+import PageTitle from "layout/PageTitle/PageTitle";
+import { Link } from "react-router-dom";
+
 const PageNotFound = () => {
   return (
-    <div>PageNotFound</div>
+    <Main variant="error">
+      <PageTitle text='UPS... page not found...' />
+      <Link to='/' className="text-3xl uppercase underline underline-offset-4">Go back home</Link>
+    </Main>
   )
 }
 
